cf78378eda7124a13178a4368b211537699a03d6
YaleKuo <yale.kuo@qq.com>
2022-07-22 23:59:06 +0800

    ListDelete成功，但还没有把e带回来

--- SqListStatic.c
+++ SqListStatic.c
@@ -35,8 +35,12 @@ bool ListDelete(SqListStatic* L, int i, int* e)
 {
 	if (L->length == 0)return false;
 	if (i<1 || i>L->length)return false;
-
-
-
-	return false;
+	e = L->data[i - 1];
+	printf("%d\n", e);
+	for (int k = i-1; k < L->length-1; k++)
+	{
+		L->data[k] = L->data[k + 1];
+	}
+	L->length--;
+	return true;
 }
--- main.c
+++ main.c
@@ -3,14 +3,23 @@
 #include "SqListDynamic.h"
 #include <stdbool.h>
 int main() {
-	printf_s("hello\n");
 	SqListStatic L;
 	InitList(&L);
 	L.data[0] = 1; L.data[1] = 2; L.data[2] = 3; L.data[3] = 5; L.data[4] = 6;
 	L.length = 5;
 	PrintList(&L);
+
 	printf("insert true or false:%d\n", ListInsert(&L, 4, 4));
 	PrintList(&L);
+
+	
+	int e = -1;
+	printf("%d\n", e);
+	printf("delete ture or false:%d\n", ListDelete(&L, 2, e));
+	printf("%d\n", e);
+	PrintList(&L);
+
+
 	return 0;
 }
 
