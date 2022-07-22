4f3d0f1665818789713e95a7140baa25a9e0f4f1
YaleKuo <yale.kuo@qq.com>
2022-07-22 23:23:22 +0800

    bool当整形输出

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "SqListStatic.h"
 #include "SqListDynamic.h"
+#include <stdbool.h>
 int main() {
 	printf_s("hello\n");
 	SqListStatic L;
@@ -8,7 +9,7 @@ int main() {
 	L.data[0] = 1; L.data[1] = 2; L.data[2] = 3; L.data[3] = 5; L.data[4] = 6;
 	L.length = 5;
 	PrintList(&L);
-	ListInsert(&L, 4, 4);
+	printf("insert true or false:%d\n", ListInsert(&L, 4, 4));
 	PrintList(&L);
 	return 0;
 }
